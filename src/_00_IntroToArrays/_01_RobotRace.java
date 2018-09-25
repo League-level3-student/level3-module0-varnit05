beae097bfe14e47f082922b2d25a3e4da1d00945
varnit05 <league@WTS14-4.attlocal.net>
2018-09-24 18:24:40 -0700

    yes

--- src/_00_IntroToArrays/_01_RobotRace.java
+++ src/_00_IntroToArrays/_01_RobotRace.java
@@ -1,30 +1,36 @@
 package _00_IntroToArrays;
 
-import java.util.Random;
-
-import javax.swing.JOptionPane;
-
 import org.jointheleague.graphical.robot.Robot;
 
 public class _01_RobotRace {
-	//1. make a main method
-	
-		//2. create an array of 5 robots.
+	// 1. make a main method
+	public static void main(String[] args) {
 
-		//3. use a for loop to initialize the robots.
+		// 2. create an array of 5 robots.
+		Robot[] bob = new Robot[5];
+		// 3. use a for loop to initialize the robots.
+		int spacex = 170;
+		for (int i = 0; i < bob.length; i++) {
+			Robot rob = new Robot();
+			rob.setX(i * spacex);
+			rob.setY(500);
+		}
+		// 4. make each robot start at the bottom of the screen, side by side, facing up
 
-			//4. make each robot start at the bottom of the screen, side by side, facing up
-	
-		//5. use another for loop to iterate through the array and make each robot move 
-	    //   a random amount less than 50.
-    	
-		//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
+		// 5. use another for loop to iterate through the array and make each robot move
+		// a random amount less than 50.
+		for (int i = 0; i < bob.length; i++) {
 
-		//7. declare that robot the winner and throw it a party!
-    	
-		//8. try different races with different amounts of robots.
-    	
-	    //9. make the robots race around a circular track.
+		}
 
-	
+		// 6. use a while loop to repeat step 5 until a robot has reached the top of the
+		// screen.
+
+		// 7. declare that robot the winner and throw it a party!
+
+		// 8. try different races with different amounts of robots.
+
+		// 9. make the robots race around a circular track.
+
+	}
 }
