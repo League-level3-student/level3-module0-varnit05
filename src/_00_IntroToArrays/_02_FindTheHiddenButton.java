1c43ca4fbbe098aa78ad660a0b14a06e07006504
varnit05 <league@WTS22.attlocal.net>
2018-12-03 18:23:48 -0800

    firework display, FindTheHiddenButton

--- src/_00_IntroToArrays/_02_FindTheHiddenButton.java
+++ src/_00_IntroToArrays/_02_FindTheHiddenButton.java
@@ -2,8 +2,7 @@ package _00_IntroToArrays;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Random;
-	
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
@@ -11,50 +10,59 @@ import javax.swing.JPanel;
 
 /* Run the FindHiddenButton.jar to see the finished product. */
 
-public class _02_FindTheHiddenButton implements ActionListener{
+public class _02_FindTheHiddenButton implements ActionListener {
 	JFrame window;
 	JPanel panel;
-	
-	//1. create an array of JButtons. Don't initialize it yet.
-	JButton [] jbut;
-	//2 create an integer variable called hiddenButton
+
+	// 1. create an array of JButtons. Don't initialize it yet.
+	JButton[] jbut;
+	// 2 create an integer variable called hiddenButton
 	int hiddenButton;
+
 	public static void main(String[] args) {
 		new _02_FindTheHiddenButton().start();
 	}
-	
+
 	public void start() {
 		window = new JFrame("Find the Button");
 		panel = new JPanel();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		//3.  Ask the user to enter a positive number and convert it to an int
-		JOptionPane.showInputDialog("type a positive integer");
-		//4. Initialize the array of JButtons to be the size of the int created in step 3
-		JButton [] jbut = hiddenButton();
-		//5. Make a for loop to iterate through the JButton array
-			//6. initialize each JButton in the array
-			//7. add the ActionListener to each JButton
-			//8. add each JButton to the panel
-		
-		//9 add the panel to the window
-		window.add(panel);
-		//10. call setExtendedState(JFrame.MAXIMIZED_BOTH) on your JFrame object.
-		window.MAXIMIZED_BOTH();
-		//11. set the JFrame to visible
-		window.setVisible(true);
-	
-		
-		//12. Give the user the instructions for the game.
-		JOptionPane.showInputDialog(null,"");
-		//13. initialize the hiddenButton variable to a random number less than the int created in step 3
-		
-		//14. Set the text of the JButton located at hiddenButton to read "ME"
 
-		//15. Use Thread.sleep(100); to pause the program.
-		
-		//16. Set the text of the JButton located at hiddenButton to be blank.
-		
+		// 3. Ask the user to enter a positive number and convert it to an int
+		JOptionPane.showInputDialog("type a positive integer");
+		// 4. Initialize the array of JButtons to be the size of the int created in step
+		// 3
+		JButton[] jbut = hiddenButton();
+		// 5. Make a for loop to iterate through the JButton array
+		for (int i = 0; i < jbut.length; i++) {
+			jbut[i] = new JButton("radish");
+			jbut[i].addActionListener(null);
+			panel.add(jbut[i]);
+		}
+		// 6. initialize each JButton in the array
+
+		// 7. add the ActionListener to each JButton
+
+		// 8. add each JButton to the panel
+
+		// 9 add the panel to the window
+		window.add(panel);
+		// 10. call setExtendedState(JFrame.MAXIMIZED_BOTH) on your JFrame object.
+
+		// 11. set the JFrame to visible
+		window.setVisible(true);
+
+		// 12. Give the user the instructions for the game.
+		JOptionPane.showInputDialog(null, "");
+		// 13. initialize the hiddenButton variable to a random number less than the int
+		// created in step 3
+
+		// 14. Set the text of the JButton located at hiddenButton to read "ME"
+
+		// 15. Use Thread.sleep(100); to pause the program.
+
+		// 16. Set the text of the JButton located at hiddenButton to be blank.
+
 	}
 
 	private JButton[] hiddenButton() {
@@ -64,10 +72,10 @@ public class _02_FindTheHiddenButton implements ActionListener{
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		JButton buttonClicked = (JButton)e.getSource();
-		
-		//17. if the hiddenButton is clicked, tell the user that they win.
+		JButton buttonClicked = (JButton) e.getSource();
 
-		//18. else tell them to try again
+		// 17. if the hiddenButton is clicked, tell the user that they win.
+
+		// 18. else tell them to try again
 	}
 }
